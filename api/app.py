3a12ea89fa95a881a68a6070b58f0651f9c40985
noriega-rafael <noriega.rafael@bcgplatinion.com>
2023-08-11 12:55:42 -0400

    refactorizacion de api

--- api/app.py
+++ api/app.py
@@ -1,70 +1,19 @@
-from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask import Flask, Blueprint
+from flask_restful import Api
 import os
-import json
 import logging as logs
-import werkzeug
-import pandas as pd
-from werkzeug.utils import secure_filename
-from google.cloud import bigquery
-from google.cloud import storage
+from resources.data_upload import DataUpload
 
 app = Flask(__name__)
+
+
+logs.basicConfig(level=logs.INFO)
+
+bp = Blueprint('api', __name__)
+
 api = Api(app)
-#TODO: add as environment
-app.config['BQ_DATASET'] = 'raw'
-app.config['PROJECT_ID'] = 'poc-globant-data'
-app.config['GCS_BUCKET'] = 'invalid_records-90c6f489'
-# Initialize BigQuery client
-client = bigquery.Client()
-
-
-
-class DataUpload(Resource):
-    section = 'upload-data'
-
-    def post(self):
-        logs.info("endpoint {}".format(self.section))
-        parser = reqparse.RequestParser()
-        parser.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files', required=True)
-        args = parser.parse_args()
-        file = args['file']
-
-        if file:
-            filename = secure_filename(file.filename)
-            table_name, extension = os.path.splitext(filename)
-       
-            try:
-        
-                    if table_name not in ['departments', 'hired_employees', 'jobs']:
-                        return {'message': f'El nombre de archivo {table_name} no es válido'}, 400
-
-                    if extension == '.csv':
-                        table_id = f'{app.config["PROJECT_ID"]}.{app.config["BQ_DATASET"]}.{table_name}'
-                        table = client.get_table(table_id)
-                        schema = table.schema
-                        df = pd.read_csv(file, sep=',', header=None, names=[field.name for field in schema] )
-
-                        # Inserta los registros en la tabla existente en BigQuery
-                        errors = client.insert_rows_json(table_id, df.to_dict(orient='records'))
-
-                        if errors:
-                            gcs_client = storage.Client()
-                            bucket = gcs_client.get_bucket(app.config['GCS_BUCKET'])
-                            blob = bucket.blob(f'failed_rows/{filename}')
-                            blob.upload_from_string(json.dumps(errors))
-
-                        return {'message': f'Registros insertados en {table_name}'}
-                    
-                    else:
-                        return {'message': 'Formato de archivo no compatible'}, 400
-                   
-                    
-                    
-
-            except Exception as e:
-                    return {'message': f'Error al procesar el archivo: {str(e)}'}, 500
-
+api.init_app(bp)
+app.register_blueprint(bp, url_prefix="/api")
 
 api.add_resource(DataUpload, '/upload')
 
